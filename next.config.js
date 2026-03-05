6318cd4a24ae334c9e311b1e9c7478d195a3b8e3
Jacob Bruce <jacobbruce@Jacobs-MacBook-Pro-2.local>
2026-03-04 18:29:50 -0600

    fix build

--- next.config.js
+++ next.config.js
@@ -1,9 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {},
-  // Allow API routes to run for up to 60s (Vercel Pro) or 10s (Hobby)
-  // For longer enrichment jobs, we use self-chaining
-  serverExternalPackages: [],
-};
-
+const nextConfig = {};
 module.exports = nextConfig;
